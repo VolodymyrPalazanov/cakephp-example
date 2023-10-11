7ab633f895476dc720d22ef5fed6d30b92209317
Volodymyr Palazanov <volodymyr.palaznov@grupa.itdesk.eu>
2023-10-11 11:11:48 +0200

    MatComponentTest works correctly

--- tests/TestCase/Controller/Component/MatComponentTest.php
+++ tests/TestCase/Controller/Component/MatComponentTest.php
@@ -1,46 +1,64 @@
 <?php
-
 declare(strict_types=1);
 
 namespace App\Test\TestCase\Controller\Component;
 
-use Cake\Controller\ComponentRegistry;
-use Cake\Controller\Component\ComponentCollection;
 use App\Controller\Component\MatComponent;
+use Cake\Controller\ComponentRegistry;
+use Cake\Controller\Controller;
+use Cake\TestSuite\IntegrationTestTrait;
 use Cake\TestSuite\TestCase;
 
 /**
- * App\Controller\Component\MatComponent Test Case.
+ * App\Controller\Component\MatComponent Test Case
  */
 class MatComponentTest extends TestCase
 {
+    use IntegrationTestTrait;
     /**
-     * Test subject.
+     * Test subject
      *
-     * @var \App\Controller\MathController
+     * @var \App\Controller\Component\MatComponent
      */
-    public $MatComponent;
+    protected $Mat;
 
     /**
-     * setUp method.
+     * setUp method
+     *
+     * @return void
      */
-    public function setUp(): void {
+    protected function setUp(): void
+    {
         parent::setUp();
-        $this->MatComponent = new MatComponent(new ComponentCollection());
+        $request = $this->getMockBuilder(\Cake\Http\ServerRequest::class)
+        ->getMock();
+        $controller = new Controller($request);
+        $registry = new ComponentRegistry($controller); // Pass the controller instance to ComponentRegistry
+
+        $this->Mat = new MatComponent($registry);
     }
 
     /**
-     * tearDown method.
+     * tearDown method
+     *
+     * @return void
      */
     protected function tearDown(): void
     {
-        unset($this->MathController);
+        unset($this->Mat);
 
         parent::tearDown();
     }
 
-    public function testAdd() {
-        $result = $this->MatComponent->add(2, 3);
+    /**
+     * Test add method
+     *
+     * @return void
+     * @uses \App\Controller\Component\MatComponent::add()
+     */
+    public function testAdd(): void
+    {
+        $result = $this->Mat->add(2,3);
         $this->assertEquals(5, $result);
     }
 }
