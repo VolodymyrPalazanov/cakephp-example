74b0a37176a4b46d778016d1c5f31458a820b7a4
Volodymyr Palazanov <volodymyr.palaznov@grupa.itdesk.eu>
2023-10-12 15:54:23 +0200

    .

--- tests/TestCase/Controller/MathControllerTest.php
+++ tests/TestCase/Controller/MathControllerTest.php
@@ -6,13 +6,13 @@ declare(strict_types=1);
  * This file is part of PHP CS Fixer.
  *
  * (c) Fabien Potencier <fabien@symfony.com>
- *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *     Dariusz Ruminski <dariusz.ruminski@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
 
- namespace App\Test\TestCase\Controller;
+namespace App\Test\TestCase\Controller;
 
 use Cake\Event\EventInterface;
 use Cake\TestSuite\Constraint\Response\StatusCode;
@@ -32,51 +32,45 @@ final class MathControllerTest extends TestCase
 {
     use IntegrationTestTrait;
 
-    /**
-     * Define the controller to use in the test
-     *
-     * @var string
-     */
-    public $controller = 'Math';
-
-    /**
-     * Disable CSRF protection for the 'add' action.
-     *
-     * @var array
-     */
-    public $csrfWhitelist = ['add'];
-
     public function setUp(): void
     {
         parent::setUp();
-        
+        // Additional setup code goes here
+    }
+
+    public function tearDown(): void
+    {
+        // Additional teardown code goes here
+        parent::tearDown();
     }
 
     /**
      * Test that the "addNumbers" action renders correctly.
      */
-    public function testAddNumbers(): void
+    public function testAddNumbers()
     {
         $this->get('/math/addNumbers');
 
         $this->assertResponseOk();
     }
-    private function getCsrfToken()
-    {
-        $token = $this->_session['request']['environment']['REQUEST_METHOD'] === 'POST' ? $this->_session['request']['data']['_csrfToken'] : '';
-        return $token;
-    }
-
 
     /**
      * Test adding numbers via the "add" action.
      */
     public function testAdd()
     {
+        $req = $this->get('/math/addNumbers');
+        $csrfToken = $req->getCookie('csrfToken')->getValue();
+
+        // Include the CSRF token in the request data
         $data = [
             'number1' => 5,
             'number2' => 7,
+            '_csrfToken' => $csrfToken,
         ];
+
+        // Manually set the CSRF token in the session
+        $this->session(['csrfToken' => $csrfToken]);
 
         $this->post('/math/add', $data);
 
@@ -86,29 +80,25 @@ final class MathControllerTest extends TestCase
 
     public function testEdit()
     {
-        $entityId = 4; // Replace with an existing ID
+        $entityId = 4;
         $data = [
-            'number1' => 10, // New values
+            'number1' => 10,
             'number2' => 15,
         ];
 
         $this->post('/math/edit/' . $entityId, $data);
 
-        // Assertions for a successful edit
         $this->assertResponseSuccess();
         $this->assertResponseContains('The math has been saved');
-        // You may also add more specific assertions as needed
     }
 
     public function testDelete()
     {
-        $entityId = 1; // Replace with an existing ID
+        $entityId = 4; 
 
         $this->post('/math/delete/' . $entityId);
 
-        // Assertions for a successful delete
         $this->assertResponseSuccess();
         $this->assertResponseContains('The math has been deleted');
-        // You may also add more specific assertions as needed
     }
 }
